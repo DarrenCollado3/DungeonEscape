0dfce37f28586a4254dd35380ee4111843682da4
DarrenCollado3 <165674655+DarrenCollado3@users.noreply.github.com>
2024-05-13 21:59:19 -0500

    interact with the doors  and start creating a system of scene transition
    after the great advance with the raycast, i decide to use the same method with other interactable objects, like doors and a new type of popup for the doors, and making the preparative for a  system of scene transition

--- DungeonScape/Assets/Scripts/Select.cs
+++ DungeonScape/Assets/Scripts/Select.cs
@@ -1,27 +1,34 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] private GameObject objeto; // GameObject a seleccionar
-    [SerializeField] private GameObject canvasPopup; // Canvas que quieres mostrar como ventana emergente
-    private bool isSelected;
+    [SerializeField] private List<GameObject> objetos; // Lista de GameObjects a seleccionar
+    [SerializeField] private List<GameObject> canvasPopups; // Lista de Canvas que quieres mostrar como ventanas emergentes
+    private bool[] isSelected; // Array de booleanos para rastrear el estado de selección de cada objeto
     public float rayLength = 1f;
     public LayerMask RaycastLayer; // Capa a la que se aplicará el rayo
     
     // Start is called before the first frame update
     void Start()
     {
-        // Desactiva el canvas al inicio
-        canvasPopup.SetActive(false);
+        // Inicializa el array de isSelected con el tamaño de la lista de objetos
+        isSelected = new bool[objetos.Count];
+
+        // Desactiva todos los canvasPopups al inicio
+        foreach (GameObject canvasPopup in canvasPopups)
+        {
+            canvasPopup.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
+        {
             Select();
+        }
     }
 
     private void Select()
@@ -32,31 +39,34 @@ public class NewBehaviourScript : MonoBehaviour
         // Dibujar el rayo en la escena
         Debug.DrawRay(transform.position, Vector2.up * rayLength, Color.red);
 
-        // Verifica si el rayo ha golpeado un objeto en la capa "raycastdetect" y si es el objeto que estamos buscando
-        if (hit.collider != null && hit.collider.gameObject == objeto)
+        // Verifica si el rayo ha golpeado un objeto en la capa "raycastdetect" y si es uno de los objetos que estamos buscando
+        for (int i = 0; i < objetos.Count; i++)
         {
-            // Cambia el color del objeto golpeado
-            if (!isSelected)
+            if (hit.collider != null && hit.collider.gameObject == objetos[i])
             {
-                isSelected = true;
-                hit.collider.GetComponent<SpriteRenderer>().color = Color.red;
-                
-                // Activa el canvas como ventana emergente
-                canvasPopup.SetActive(true);
-                
-                // Mensaje de depuración
-                Debug.Log("Objeto seleccionado: " + objeto.name);
-            }
-            else
-            {
-                isSelected = false;
-                hit.collider.GetComponent<SpriteRenderer>().color = Color.white; // Cambiar al color predeterminado cuando no está seleccionado
-                
-                // Desactiva el canvas
-                canvasPopup.SetActive(false);
-                
-                // Mensaje de depuración
-                Debug.Log("Objeto deseleccionado: " + objeto.name);
+                // Cambia el color del objeto golpeado
+                if (!isSelected[i])
+                {
+                    isSelected[i] = true;
+                    hit.collider.GetComponent<SpriteRenderer>().color = Color.red;
+                    
+                    // Activa el canvas correspondiente como ventana emergente
+                    canvasPopups[i].SetActive(true);
+                    
+                    // Mensaje de depuración
+                    Debug.Log("Objeto seleccionado: " + objetos[i].name);
+                }
+                else
+                {
+                    isSelected[i] = false;
+                    hit.collider.GetComponent<SpriteRenderer>().color = Color.white; // Cambiar al color predeterminado cuando no está seleccionado
+                    
+                    // Desactiva el canvas correspondiente
+                    canvasPopups[i].SetActive(false);
+                    
+                    // Mensaje de depuración
+                    Debug.Log("Objeto deseleccionado: " + objetos[i].name);
+                }
             }
         }
     }
